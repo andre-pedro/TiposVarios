1096dab8ec4c674cfc0a1050eacfb35dbf24225e
hardcorelif3 <33433474+hardcorelif3@users.noreply.github.com>
2018-02-22 15:39:53 +0000

    Add strings verbatim e interpolada

--- TiposVarios/Program.cs
+++ TiposVarios/Program.cs
@@ -15,9 +15,21 @@ namespace TiposVarios
             string verbatim = @"C:\Users\Ana\Documents";
             string normal = "dfsfdfsdfs";
             string verbaspas = @"""Pls Work""";
+            string concatenada = "Uma " + "String " + "Concatenada";
+            string concatenada2 = "Mood: " + normal + verbaspas;
+            //concatenada += " texto";
+            string sendhelp = $"{concatenada} mais {normal} e' igual a {concatenada + normal}";
+            string verbinter = $@"Sou uma string verbatim interpolada, {verbaspas}";
+
             Console.WriteLine(verbatim);
             Console.WriteLine(normal);
             Console.WriteLine(verbaspas);
+            Console.WriteLine(concatenada);
+            Console.WriteLine(concatenada2);
+            Console.WriteLine(sendhelp);
+            Console.WriteLine(verbinter);
+            
+            
 
         }
     }
